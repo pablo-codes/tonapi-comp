fd4a667756347198f3b3e84871622101e4a11103
pablo-codes <112553944+pablo-codes@users.noreply.github.com>
2025-07-12 10:52:09 +0100

    resolving deploy pages polyfill error

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,43 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+// esbuild plugins for dev‑time polyfills
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+
+// Rollup polyfills for build‑time
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
   plugins: [
+
+    nodePolyfills(),
     react(),
-    nodePolyfills({
-      include: ['buffer', 'process'],
-      globals: {
-        Buffer: true,
-        global: true,
-        process: true,
-      },
-    })
   ],
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({ buffer: true, process: true }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+
+  resolve: {
+    alias: {
+      process: 'rollup-plugin-node-polyfills/polyfills/process-es6',
+      buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
+    },
+  },
   define: {
     global: 'globalThis',
   },
+
   server: {
     port: 3000,
-    open: true
-  }
+    open: true,
+  },
 })
