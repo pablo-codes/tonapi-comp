f5acddc79e91a7e441c7efbdcd07966cbbcfe439
pablo-codes <112553944+pablo-codes@users.noreply.github.com>
2025-07-12 11:31:44 +0100

    vite config polyfill issues test 3

--- vite.config.js
+++ vite.config.js
@@ -7,15 +7,6 @@ export default defineConfig({
     react(),
     nodePolyfills(),
   ],
-  resolve: {
-    alias: {
-      buffer: 'buffer',      // Use the buffer npm package
-      process: 'process',    // Use the process npm package
-    },
-  },
-  define: {
-    global: 'globalThis',    // This is important for Buffer
-  },
   server: {
     port: 3000,
     open: true,
