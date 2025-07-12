2abdd75678b29225086cb0c8acf02bd47b17e2be
pablo-codes <112553944+pablo-codes@users.noreply.github.com>
2025-07-12 11:17:06 +0100

    vite config polyfill issues test 2

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-
 
 export default defineConfig({
   plugins: [
-
-    nodePolyfills(),
     react(),
+    nodePolyfills(),
   ],
-
-  optimizeDeps: {
-    esbuildOptions: {
-      define: { global: 'globalThis' },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({ buffer: true, process: true }),
-
-      ],
-    },
-  },
-
   resolve: {
     alias: {
-      process: 'rollup-plugin-node-polyfills/polyfills/process-es6',
-      buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
+      buffer: 'buffer',      // Use the buffer npm package
+      process: 'process',    // Use the process npm package
     },
   },
-
-  build: {
-    commonjsOptions: {
-      include: [
-        /node_modules/,
-        /rollup-plugin-node-polyfills/,
-      ],
-    },
-  },
-
-  // 6) Global‐this shim
   define: {
-    global: 'globalThis',
+    global: 'globalThis',    // This is important for Buffer
   },
-
   server: {
     port: 3000,
     open: true,
